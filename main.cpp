fcad9213dc67139a279714c5a136b42950912784
olddunk <50358709+olddunk@users.noreply.github.com>
2025-05-30 08:49:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,37 @@
 #include <iostream>
-#include "User.h"
-#include "App.h"
-#include "Admin.h"
-#include "Customer.h"
-#include "Doctor.h"
-
+#include <iomanip>
+#include <string>
+#include "Shape.h"
+#include "Circle.h"
+#include "Square.h"
 using namespace std;
 
-User* login(const string& filename) {
-    string username, password;
-    cout << "Login\nUsername: ";
-    cin >> username;
-    cout << "Password: ";
-    cin >> password;
-
-    auto users = User::readUsersFromFile(filename);
-    for (auto& user : users) {
-        if (user.getName() == username && user.getPassword() == password) {
-            cout << "Login successful!\n";
-            return new User(user);  // Return a copy of the matched user
-        }
-    }
-
-    cout << "Login failed.\n";
-    return nullptr;
+void display(Shape& s) {
+    cout << "顏色：" << s.getColor() << endl;
+    cout << fixed << setprecision(1) << "體積：" << s.getVolume()<< endl;
 }
 
-int main() {
-    string filename = "user.txt";
-    User* loggedUser = login(filename);
-    if (!loggedUser) return 0;
+int main()
+{   string color1, color2;
+    double radius, edge;
+    cin >> radius;
+    cin >> color1;
+    cin >> edge;
+    cin >> color2;
     
-    App app(*loggedUser);
-    // cout << "Welcome! " << app.currentUser.name << endl;
-    app.run();
-    // switch (loggedUser->role) {
-    //     case Role::Admin: {
-    //         Admin admin(loggedUser->id, loggedUser->getName(), loggedUser->getPassword(), loggedUser->tel);
-    //         admin.run();
-    //         break;
-    //     }
+    Circle c1(radius, color1);
+    cout << "=== [圓形] ===" << endl;
+    cout << "半徑：" << c1.getRadius() << endl;
+    display(c1);
+    // cout << "顏色：" << c1.getColor() << endl;
+    // cout << fixed << setprecision(1) << "體積：" << c1.getVolume()<< endl;
+    
+    Square s1(edge, color2);
+    cout << "=== [正方形] ===" << endl;
+    cout << "邊長：" << s1.getEdge() << endl;
+    display(s1);
+    // cout << "顏色：" << s1.getColor() << endl;
+    // cout << fixed << setprecision(1) << "體積：" << s1.getVolume()<< endl;
 
-    //     case Role::Customer:
-    //         cout << "Welcome, customer!\n";
-    //         break;
-    //     case Role::Doctor:
-    //         cout << "Welcome, doctor!\n";
-    //         break;
-    // }
-
-    app.logout();
-    delete loggedUser;
     return 0;
 }
